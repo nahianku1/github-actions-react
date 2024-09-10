f9ee78f4db6db251cd6387787e05d272bb815361
himel <himelku1@gmail.com>
2024-09-10 13:21:39 +0600

    main changed again 7

--- .github/actions/deploy-to-netlify/main.js
+++ .github/actions/deploy-to-netlify/main.js
@@ -8,12 +8,42 @@ async function run() {
 
     process.env.NETLIFY_AUTH_TOKEN = authToken;
 
-    exec(
-      `npm ci && npm run build && npx netlify deploy --dir=dist --site=${siteName} --prod`
+    const execCommand = (cmd) => {
+      return new Promise((resolve, reject) => {
+        exec(cmd, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error: ${error.message}`);
+            reject(error);
+          } else {
+            if (stderr) {
+              console.warn(`Warning: ${stderr}`);
+            }
+            resolve(stdout);
+          }
+        });
+      });
+    };
+
+    //install dependencies
+    console.log("Installing Dependencies...");
+    const installOutput = await execCommand(`npm ci`);
+    console.log(installOutput);
+
+    //Building artifacts
+    console.log("Building artifacts...");
+    const buildOutput = await execCommand(`npm run build`);
+    console.log(buildOutput);
+
+    // Deploy to Netlify
+    console.log("Deploying to Netlify...");
+    const deployOutput = await execCommand(
+      `npx netlify deploy --dir=${buildDir} --site=${siteName} --prod`
     );
+    console.log(deployOutput);
+
+    console.log("Deployment to Netlify was successful.");
   } catch (error) {
     core.setFailed(error.message);
   }
 }
-
 run();
