02b1f2b9611451974c1b6ae1e30a4aef6c33209b
himel <himelku1@gmail.com>
2024-09-10 13:42:02 +0600

    main changed again 14

--- .github/actions/deploy-to-netlify/main.js
+++ .github/actions/deploy-to-netlify/main.js
@@ -1,5 +1,5 @@
 const core = require("@actions/core");
-const { exec } = require("@actions/exec");
+const { execSync } = require("child_process");
 
 async function run() {
   try {
@@ -11,38 +11,17 @@ async function run() {
     // Set environment variables for Netlify CLI
     process.env.NETLIFY_AUTH_TOKEN = authToken;
 
-    // Function to execute shell commands
-    const execCommand = (cmd) => {
-      return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-          if (error) {
-            console.error(`Error: ${error.message}`);
-            reject(error);
-          } else {
-            if (stderr) {
-              console.warn(`Warning: ${stderr}`);
-            }
-            resolve(stdout);
-          }
-        });
-      });
-    };
+    // Install Dependencies
+    console.log("Installing Dependencies...");
+    execSync(`npm ci`);
 
-     // Install Dependencies
-     console.log("Installing Dependencies...");
-     await execCommand(`npm ci`);
- 
-     // Building artifacts
-     console.log("Building artifacts...");
-     const createSiteOutput = await execCommand(`npm run build`);
-     console.log(createSiteOutput);
+    // Building artifacts
+    console.log("Building artifacts...");
+    const createSiteOutput = execSync(`npm run build`);
 
     // Deploy to Netlify
     console.log("Running deployment commands...");
-    const deployOutput = await execCommand(
-      `netlify deploy --dir=${buildDir} --site=${siteName} --prod`
-    );
-    console.log(deployOutput);
+    execSync(`netlify deploy --dir=${buildDir} --site=${siteName} --prod`);
 
     console.log("Deployment to Netlify was successful.");
   } catch (error) {
