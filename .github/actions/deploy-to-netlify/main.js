850cd8aaf9b10d7cc6233d97a6bb564a055fae28
himel <himelku1@gmail.com>
2024-09-10 12:40:59 +0600

    main changed

--- .github/actions/deploy-to-netlify/main.js
+++ .github/actions/deploy-to-netlify/main.js
@@ -1,45 +1,17 @@
 const core = require("@actions/core");
 const { exec } = require("@actions/exec");
+const { getOctokit, context } = require("@actions/github");
 
 async function run() {
-  try {
-    // Get inputs from the action metadata
-    const authToken = core.getInput("auth_token");
-    const buildDir = core.getInput("build_directory");
-    const siteName = core.getInput("site_name");
+    const token = core.getInput("token");
 
-    // Set environment variables for Netlify CLI
-    process.env.NETLIFY_AUTH_TOKEN = authToken;
+    const octokit = getOctokit(token);
 
-    // Function to execute shell commands
-    const execCommand = (cmd) => {
-      return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-          if (error) {
-            console.error(`Error: ${error.message}`);
-            reject(error);
-          } else {
-            if (stderr) {
-              console.warn(`Warning: ${stderr}`);
-            }
-            resolve(stdout);
-          }
-        });
-      });
-    };
+    const { actor, repo } = context;
 
-    // Deploy to Netlify
-    console.log("Running deployment commands...");
-    const deployOutput = await execCommand(
-      `netlify deploy --dir=${buildDir} --site=${siteName} --prod`
-    );
-    console.log(deployOutput);
-    console.log(process.env.NETLIFY_AUTH_TOKEN);
+    console.log({actor, repo});
 
-    console.log("Deployment to Netlify was successful.");
-  } catch (error) {
-    core.setFailed(`Action failed with error: ${error.message}`);
-  }
+   
 }
 
 run();
